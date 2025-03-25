f74f648bf50c9ac7a50045ad04a2eeca41d84fc8
ZackHan-dot <15373303031@163.com>
2025-03-25 22:24:47 +0800

    feat: nuxt3.x template, element-plus

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        <NuxtRouteAnnouncer />
+        <NuxtWelcome />
+        <el-button @click="ElMessage('hello')">button</el-button>
+    </div>
+</template>
